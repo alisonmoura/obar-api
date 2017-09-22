c560dbfe5b3ce819fa0225d81a04c5803ee73839
Alison Moura <alison.oghino@gmail.com>
2017-09-22 19:22:19 -0400

    Configuração inicial do projeto

---
+++ src/main/java/com/obar/ObarApiApplication.java
@@ -0,0 +1,12 @@
+package com.obar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObarApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/obar/ObarApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ObarApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
